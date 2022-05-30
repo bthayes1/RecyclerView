350ad10c3d1ee5594a4b5109311fb3ea6c8ef597
bthayes1 <79590455+bthayes1@users.noreply.github.com>
2022-05-30 08:33:37 -0400

    Implemented Heterogenous views. Example of a RecyclerView with Ads

--- app/src/main/java/com/example/recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/recyclerview/MainActivity.kt
@@ -1,21 +1,58 @@
 package com.example.recyclerview
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.snackbar.Snackbar
 
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val constraintLayout = findViewById<ConstraintLayout>(R.id.layout)
+        val data = createData()
         val rvContact = findViewById<RecyclerView>(R.id.rvContacts)
-        rvContact.adapter = ContactAdapter(this, createData())
+        val adapter = ContactAdapter(this, data)
+        rvContact.adapter = adapter
         rvContact.layoutManager = LinearLayoutManager(this)
+
+        val swipeToDelete = object : SwipeToDelete(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.bindingAdapterPosition
+                val currentData = data[position]
+                Log.i(TAG, "Item swiped at position: $position")
+                data.removeAt(position)
+                adapter.notifyItemRemoved(position)
+                Snackbar.make(constraintLayout, "Item was deleted", Snackbar.LENGTH_LONG)
+                    .setAction("Undo", UndoListener(position, currentData))
+                    .setActionTextColor(Color.WHITE)
+                    .show()
+            }
+
+            inner class UndoListener(private val position: Int, private val deletedItem: Any) :
+                View.OnClickListener{
+
+                override fun onClick(p0: View?) {
+                    data.add(position, deletedItem)
+                    adapter.notifyItemInserted(position)
+                    Log.i(TAG, "onClick: Undo was pressed. Data was added at position: $position")
+                }
+            }
+        }
+        val itemTouchHelper = ItemTouchHelper(swipeToDelete)
+        itemTouchHelper.attachToRecyclerView(rvContact)
     }
 
-    private fun createData() : List<Any>{
+
+
+    private fun createData() : MutableList<Any>{
         val list = mutableListOf<Any>()
         for (i in 0..150){
             val rand = (22..70).random()
--- app/src/main/java/com/example/recyclerview/SwipeToDelete.kt
+++ app/src/main/java/com/example/recyclerview/SwipeToDelete.kt
@@ -1,4 +1,30 @@
 package com.example.recyclerview
 
-class SwipeToDelete {
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
+
+abstract class SwipeToDelete : ItemTouchHelper.Callback() {
+
+    /**
+     * Notifies if a item has received a swipe
+     */
+    override fun getMovementFlags(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder
+    ): Int {
+        val swipeFlag = ItemTouchHelper.LEFT
+        return makeMovementFlags(0, swipeFlag)
+    }
+
+    /**
+     * onMove is only needed if drag and drop functionality is needed
+     * in the recyclerView
+     */
+    override fun onMove(
+        recyclerView: RecyclerView,
+        viewHolder: RecyclerView.ViewHolder,
+        target: RecyclerView.ViewHolder
+    ): Boolean {
+        return false
+    }
 }
